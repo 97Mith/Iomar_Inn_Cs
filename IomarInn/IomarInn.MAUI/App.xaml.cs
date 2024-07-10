392efa053e3db0ff97cb9d3b4bdb499fad03a773
Matheus Tarasczuk <mathtarasczuk@gmail.com>
2024-07-10 15:36:05 -0300

    Created Projects: Domain, Application, Infra.Data, Infra.IoC e MAUI

---
+++ IomarInn/IomarInn.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IomarInn.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ IomarInn/IomarInn.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IomarInn.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
