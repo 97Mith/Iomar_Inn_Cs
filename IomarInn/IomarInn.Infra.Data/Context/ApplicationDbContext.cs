b54239d14ea365e7246c22456713334c39cdae20
Matheus Tarasczuk <mathtarasczuk@gmail.com>
2024-07-14 21:00:29 -0300

    Create: Infra.Data: Context: ApplicationDbContext: Instalação do pacote DbContext e perifericos e criação da classe

---
+++ IomarInn/IomarInn.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IomarInn.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IomarInn.Infra.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options) 
+    { }
+
+    //define a entidade que será mapeada e o nome da tabela
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
--- IomarInn/IomarInn.MAUI/obj/Debug/net8.0-android/IomarInn.MAUI.AssemblyInfo.cs
+++ IomarInn/IomarInn.MAUI/obj/Debug/net8.0-android/IomarInn.MAUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IomarInn.MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+618c16b432895005ab9ecc218fa1db3eaff17143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+790216c73a8fc04114acf07713237a50754bfa74")]
 [assembly: System.Reflection.AssemblyProductAttribute("IomarInn.MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IomarInn.MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
